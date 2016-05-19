be8bf93ac30b291066acc222ef0b990e156606a1
dingxiaohuan <wydingxiaohuan@163.com>
2016-05-19 11:30:42 +0800

    php json 反编码2  自定义json字符串单双引号有要求 php版权问题

--- json/2.php
+++ json/2.php
@@ -3,8 +3,16 @@
 $city=array('hebei'=>'shijiazhuang','shandong'=>'jinan','henan'=>'zhengzhou');
 $jn_city=json_encode($city);
 //反编码
-$fan_city=json_decode($jn_city);
-var_dump($fan_city);//object(stdClass)#1 (3) { ["hebei"]=> string(12) "shijiazhuang" ["shandong"]=> string(5) "jinan" ["henan"]=> string(9) "zhengzhou" }
-
+//$fan_city=json_decode($jn_city);
+//var_dump($fan_city);//object(stdClass)#1 (3) { ["hebei"]=> string(12) "shijiazhuang" ["shandong"]=> string(5) "jinan" ["henan"]=> string(9) "zhengzhou" }
+//
 $fan_city1=json_decode($jn_city,true);//以数组形式反编码
 var_dump($fan_city1);
+
+//给一个自定义的json字符串反编码操作
+//反编码的json字符串对定义有单双引号有要求
+//$jn_str="{name:'kitty',color:'gray',age:5}";//null
+//$jn_str="{'name':'kitty','color':'gray','age':'5'}";//NULL
+$jn_str='{"name":"kitty","color":"gray","age":"5"}';
+$fan_str=json_decode($jn_str,true);
+var_dump($fan_str);
