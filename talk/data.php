1b93e9a942ca83e4df94b26e4a6c6ea89a12bcc4
dingxiaohuan <wydingxiaohuan@163.com>
2016-05-19 21:41:05 +0800

    ajax聊天室2

--- talk/data.php
+++ talk/data.php
@@ -4,8 +4,11 @@ header("Content-type:text/html;charset=utf-8");
 $link=mysql_connect('localhost','root','123') or exit(mysql_error());
 mysql_select_db('shop0609',$link);
 mysql_query('set names utf8');
+
+$maxID=$_GET['maxID'];
 //每次都请求新的聊天内容
-$sql="select * from message";
+//本次请求的记录结果id需要大于上次已经获得的记录的最大id
+$sql="select * from message WHERE id >'$maxID'";
 $qry=mysql_query($sql);
 $info=array();
 while($rst=mysql_fetch_assoc($qry)){
